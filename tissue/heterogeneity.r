baffddb88ffc2983b2afcca221676c7c83e50f3e
Gibran Hemani <explodecomputer@gmail.com>
2021-06-25 16:46:18 +0100

    updated heterogeneity results

--- tissue/heterogeneity.r
+++ tissue/heterogeneity.r
@@ -2,7 +2,7 @@ library(tidyverse)
 library(meta)
 library(parallel)
 
-load("/projects/MRC-IEU/research/projects/ieu2/p5/091/working/data/tissue_specificity/tissue.RData")
+load("tissue.RData")
 
 blood <- clumped %>%
 	select(cpg=cpg, snp=snp, id=id, beta_blood=Effect, se_blood=StdErr, cis=cis)
@@ -20,6 +20,7 @@ fn <- function(r)
 {
 	a <- metagen(c(r$beta_blood, r$beta_brain, r$beta_adipose), c(r$se_blood, r$se_brain, r$se_adipose))
 	tibble(
+		id = r$id,
 		nstudies = a$k,
 		Q = a$Q,
 		Qpval = a$pval.Q,
@@ -31,8 +32,9 @@ fn <- function(r)
 out <- mclapply(1:nrow(dat), function(i) {
 	message(i)
 	fn(dat[i, ])
-	}, mc.cores=10) %>%
-	bind_rows()
+	}, mc.cores=16) %>%
+	bind_rows() %>%
+	inner_join(dat, ., by="id")
 
 
 save(out, file="heterogeneity.rdata")
