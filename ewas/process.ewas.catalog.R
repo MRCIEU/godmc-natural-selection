81fb7cc5c7c4b6904fc54a9b55af5ffa1ce9c23c
Josine Min <josine.min@bristol.ac.uk>
2021-09-24 17:39:42 +0100

    ewas

--- ewas/process.ewas.catalog.R
+++ ewas/process.ewas.catalog.R
@@ -7,12 +7,19 @@ length(unique(r$StudyID))
 #[1] 692 #1998
 nrow(data.frame(table(rmeta$Exposure)))
 #[1] 647 #1835
+studyid<-unique(r$StudyID)
+rmeta<-rmeta[which(rmeta$StudyID%in%studyid),]
+length(unique(rmeta$StudyID)) #1998
+length(unique(r$StudyID)) #1998
 
 g1<-grep("whole blood",rmeta$Tissue,ignore.case =T)
 table(rmeta[g1,"Tissue"])
 rmeta<-rmeta[g1,]
 studyid<-unique(rmeta$StudyID)
 r<-r[which(r$StudyID%in%studyid),]
+
+length(unique(rmeta$StudyID))
+#1804
 length(unique(r$StudyID))
 #[1] 1804
 
@@ -25,17 +32,28 @@ studyid<-unique(rmeta$StudyID)
 r<-r[which(r$StudyID%in%studyid),]
 length(unique(r$StudyID))
 # [1] 1754
+length(unique(rmeta$StudyID))
+#[1] 1754
 
 rmeta<-rmeta[rmeta$N>1000,]
 studyid<-unique(rmeta$StudyID)
 r<-r[which(r$StudyID%in%studyid),]
+studyid<-unique(r$StudyID)
+rmeta<-rmeta[which(rmeta$StudyID%in%studyid),]
+length(unique(rmeta$StudyID))
+#1462
 length(unique(r$StudyID))
 #1462
 nrow(data.frame(table(rmeta$Exposure)))
-#1254
+#1203
+nrow(data.frame(table(rmeta$Outcome)))
+#114
 
+nrow(r)
+nrow(rmeta)
 m<-match(r$StudyID,rmeta$StudyID)
 rmeta2<-data.frame(rmeta[m,])
+nrow(rmeta2)
 r<-data.frame(r)
 
 r<-data.frame(r,Outcome=rmeta2[,"Outcome"],Exposure=rmeta2[,"Exposure"])
@@ -289,7 +307,7 @@ r$subcategory[g]<-"Adversity"
 
 
 g<-grep("bone",r$StudyID)
-r$subcategory[g]<-"Bone"
+r$subcategory[g]<-"Bone mineral density"
 
 #g<-grep("acetate",r$StudyID)
 #r$subcategory[g]<-"AminoAcid"
@@ -320,8 +338,6 @@ g<-grep("human_immunodeficiency_virus",r$StudyID)
 r$subcategory[g]<-"Infectious Disease"
 g<-grep("acquired_hiv",r$StudyID)
 r$subcategory[g]<-"Infectious Disease"
-
-
 g<-grep("hepatitis_c",r$StudyID)
 r$subcategory[g]<-"Infectious Disease"
 
@@ -370,7 +386,6 @@ g<-grep("Wang-Y_aging",r$StudyID)
 r$subcategory[g]<-"Aging"
 g<-grep("Jansen-R_age",r$StudyID)
 r$subcategory[g]<-"Aging"
-
 g<-grep("leukocyte_telomere_length",r$StudyID)
 r$subcategory[g]<-"Aging"
 g<-grep("mortality",r$StudyID)
@@ -486,8 +501,6 @@ r$subcategory[g]<-"Thyroid function"
 g<-grep("thyrotropin",r$StudyID)
 r$subcategory[g]<-"Thyroid function"
 
-
-
 g<-grep("atopy",r$StudyID)
 r$subcategory[g]<-"Allergy"
 g<-grep("asthma",r$StudyID)
@@ -599,11 +612,8 @@ r$subcategory[g]<-"Education"
 g<-grep("cognitive_abilities",r$StudyID)
 r$subcategory[g]<-"Education"
 
-
-
 g<-grep("31062658_Santos",r$StudyID)
 r$subcategory[g]<-"Social"
-
 
 g<-grep("thalamus_volume",r$StudyID)
 r$subcategory[g]<-"Brain trait"
@@ -644,8 +654,7 @@ length(r$CpG)
 length(unique(r$CpG))
 #[1] 312820
 
-w<-which(r$subcategory%in%c("Metabolite","Gene expression","mi-RNA","Tissue","Brain trait"))
-r<-r[-w,]
+save(r,rmeta,file="ewas_category.Robj")
 
 table(r$subcategory)
 
@@ -653,50 +662,126 @@ table(r$subcategory)
 #                    59                     22                 470614 
 #         Air pollution         Anthropometric            Behavioural 
 #                  7262                   1473                  14751 
-#                  Bone                 Cancer         Cardiovascular 
-#                     3                   1872                     39 
-#              Cytokine                   Diet              Education 
-#                  175                     25                    166 
-#            Glycemic           Hypertension      IgG glycosylation 
-#                   127                    160                      6 
-#   Immune cell subset          Immune system                 Kidney 
-#                    13                    217                     99 
-#                 Lipid                  Liver          Lung function 
-#                   648                     51                    125 
-#                Metals Neurological disorders       Oxidative Stress 
-#                     4                     13                    106 
-# Psychiatric disorders           Reproductive            Respiratory 
-#                  2301                   4328                     41 
-#                   Sex       Thyroid function               Vitamins 
-#                 36052                      7                      2 
+#  Bone mineral density            Brain trait                 Cancer 
+#                     3                      2                   1872 
+#        Cardiovascular               Cytokine                   Diet 
+#                    39                    175                     25 
+#             Education        Gene expression               Glycemic 
+#                   166                   3418                    127 
+#          Hypertension      IgG glycosylation     Immune cell subset 
+#                   160                      6                     13 
+#         Immune system                 Kidney                  Lipid 
+#                  217                     99                    648 
+#                 Liver          Lung function             Metabolite 
+#                    51                    125                   2673 
+#                Metals                 mi-RNA Neurological disorders 
+#                     4                    123                     13 
+#      Oxidative Stress  Psychiatric disorders           Reproductive 
+#                   106                   2301                   4328 
+#           Respiratory                    Sex       Thyroid function 
+#                   41                  36052                      7 
+#             Vitamins 
+#                   2 
 
-outcpg <- r %>% 
+#w<-which(r$subcategory%in%c("Metabolite","Gene expression","mi-RNA","Tissue","Brain trait"))
+#r<-r[-w,]
+nrow(r) #546998
+w<-which(r$subcategory%in%c("Adversity","Age by sex interaction","Aging","Air pollution","Behavioural","Diet","Gene expression","Metabolite","IgG glycosylation","Metals","mi-RNA","Oxidative Stress","Sex","Vitamins"))
+r2<-r[-w,]
+nrow(r2)
+#11881
+
+g<-grep("Cancer treatment",r2$Phenotype)
+r2<-r2[-g,]
+g<-grep("APOE e2 vs e4",r2$Phenotype)
+r2<-r2[-g,]
+g<-grep("Dichlorodiphenyldichloroethylene",r2$Phenotype)
+r2<-r2[-g,]
+g<-grep("Statin use",r2$Phenotype)
+r2<-r2[-g,]
+
+nrow(r2)
+#10487
+
+r2$Phenotype<-gsub("Incident atrial fibrillation","Atrial fibrillation",r2$Phenotype)
+r2$Phenotype<-gsub("Prevalent atrial fibrillation","Atrial fibrillation",r2$Phenotype)
+r2$Phenotype<-gsub("Lp(a)","Lipoprotein(a) levels",r2$Phenotype,fixed=T)
+r2$Phenotype<-gsub("Serum high-density lipoprotein cholesterol","High-density lipoprotein cholesterol",r2$Phenotype)
+r2$Phenotype<-gsub("Serum low-density lipoprotein cholesterol","Low-density lipoprotein cholesterol",r2$Phenotype)
+r2$Phenotype<-gsub("Serum total cholesterol","Total cholesterol",r2$Phenotype)
+r2$Phenotype<-gsub("Serum triglycerides","Triglycerides",r2$Phenotype)
+r2$Phenotype<-gsub("Type II diabetes","Type 2 diabetes",r2$Phenotype)
+r2$Phenotype<-gsub("Waist circumfrence","Waist circumference",r2$Phenotype)
+r2$Phenotype<-gsub("FEV1 / FVC","FEV1/FVC",r2$Phenotype,fixed=T)
+r2$Phenotype<-gsub("Cognitive abilities : digit test","Cognitive abilities: digit test",r2$Phenotype,fixed=T)
+r2$Phenotype<-gsub("fasting glucose","Fasting glucose",r2$Phenotype)
+r2$Phenotype<-gsub("fasting insulin","Fasting insulin",r2$Phenotype)
+r2$Phenotype<-gsub("body mass index","Body mass index",r2$Phenotype)
+
+
+df<-data.frame(table(r2$Phenotype))
+nrow(df)
+#[1] 56
+
+df2<-data.frame(table(r2$subcategory))
+nrow(df2)
+# 20
+
+data.frame(df2)
+#                     Var1 Freq
+#1          Anthropometric 1473
+#2    Bone mineral density    3
+#3             Brain trait    2
+#4                  Cancer  506
+#5          Cardiovascular   39
+#6                Cytokine  175
+#7               Education  166
+#8                Glycemic  127
+#9            Hypertension  160
+#10     Immune cell subset   13
+#11          Immune system  217
+#12                 Kidney   99
+#13                  Lipid  641
+#14                  Liver   51
+#15          Lung function  125
+#16 Neurological disorders   13
+#17  Psychiatric disorders 2301
+#18           Reproductive 4328
+#19            Respiratory   41
+#20       Thyroid function    7
+
+
+outcpg <- r2 %>% 
   filter(!is.na(P)) %>%
   group_by(CpG) %>%
-  summarise(
-    n=n(), 
+  summarise( 
     nsig=sum(P < 1e-7),
     minP=min(P),
     maxP=max(P),
     pheno_uniq=paste(unique(Phenotype),collapse=";"),
-    phenocategory_uniq=paste(unique(subcategory),collapse=";")
-
+    phenocategory_uniq=paste(unique(subcategory),collapse=";"),
+    npheno=length(unique(Phenotype)),
+    nphenocategory=length(unique(subcategory))
   )
 str(outcpg)
+nrow(outcpg)
+#7138
 
 outcpg[which(outcpg$nsig>10),]
 
 save(outcpg,file="~/repo/godmc-natural-selection/ewas/outcpg.rdata")
 
 load("/projects/MRC-IEU/research/projects/ieu2/p5/091/working/data/tfbs/tfbsbycpg_summarised.Robj")
-names(outcpg)<-c("cpg","n_ewas","n_ewas_sig","ewas_minP","ewas_max_P","ewas_pheno_uniq","ewas_pheno_category_uniq")
+names(outcpg)<-c("cpg","n_ewas_sig","ewas_minP","ewas_max_P","ewas_pheno_uniq","ewas_pheno_category_uniq","ewas_npheno","ewas_nphenocategory")
 m<-match(tfbs2$cpg,outcpg$cpg)
 tfbs2<-data.frame(tfbs2,outcpg[m,])
 
 length(which(is.na(tfbs2$ewas_pheno_category_uniq)))
-#[1] 52135
+#[1] 185957
 length(which(!is.na(tfbs2$ewas_pheno_category_uniq)))
-#[1] 137967
+#[1] 4145
 
 save(tfbs2,file="/projects/MRC-IEU/research/projects/ieu2/p5/091/working/data/tfbs/tfbsbycpg_summarised.Robj")
+nrow(tfbs2)
 
+###
